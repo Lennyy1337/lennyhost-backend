d60f80343d6b7de9db7761fc1796699dd5bd800e
Lennyy1337 <lennyygameplay@gmail.com>
2024-03-30 16:07:30 +0100

    Add dockerfile

--- build/init/jwt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwt = void 0;
-const jwtToken_1 = require("../tools/jwtToken");
-exports.jwt = new jwtToken_1.jwtToken(process.env.JWT_SECRET);
