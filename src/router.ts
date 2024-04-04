31ea9533aa6132d5f22259c7a4825161befa3517
Lennyy1337 <lennyygameplay@gmail.com>
2024-04-04 18:40:57 +0200

    remove depreciated shareX route

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,6 @@ import { registerRoute } from "./routes/auth/register";
 import { generateKey } from "./routes/main/key/generateKey";
 import { getUploadedFilesRoute } from "./routes/main/getuploadedfiles";
 import { getSxcuRoute } from "./routes/main/sharex/getsxcu/getSxcu";
-import { uploadShareXRoute } from "./routes/main/sharex/shareXupload";
 import { uploadRoute } from "./routes/main/upload";
 import { redeemKey } from "./routes/main/key/redeemKey";
 
@@ -13,7 +12,6 @@ export function router() {
   fastify.register(loginRoute);
   fastify.register(uploadRoute);
   fastify.register(getUploadedFilesRoute);
-  //fastify.register(uploadShareXRoute)
   fastify.register(getSxcuRoute);
   fastify.register(generateKey);
   fastify.register(redeemKey);
